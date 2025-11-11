e437642fd59fad6bb62b648aede802d5713eaea9
Fabian Heflo <fheflo@gmail.com>
2025-11-11 16:11:09 +0100

    with gif

--- sinus_app.py
+++ sinus_app.py
@@ -39,3 +39,71 @@ if st.session_state.playing and frequency != st.session_state.prev_freq:
     wave = np.sin(2 * np.pi * frequency * t)
     sd.play(wave, samplerate=sample_rate, loop=True)
     st.session_state.prev_freq = frequency
+
+
+st.markdown("---")  # skillelinje
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+
+# Felles parametre
+x = np.linspace(0, 4 * np.pi, 400)
+fps = 20
+frames = 60
+
+def make_interference_gif(mode, filename):
+    fig, ax = plt.subplots(figsize=(6, 3))
+    ax.set_ylim(-2.5, 2.5)
+    ax.set_xlim(0, 4 * np.pi)
+    ax.axis('off')
+
+    line1, = ax.plot([], [], lw=2, color='blue')
+    line2, = ax.plot([], [], lw=2, color='red')
+    line_sum, = ax.plot([], [], lw=2.5, color='black')
+
+    def init():
+        line1.set_data([], [])
+        line2.set_data([], [])
+        line_sum.set_data([], [])
+        return line1, line2, line_sum
+
+    def animate(i):
+        t = i / fps
+        if mode == "constructive":
+            # To forskjellige bølger (forskjellig frekvens)
+            y1 = np.sin(1.0 * x - 2 * np.pi * 0.4 * t)
+            y2 = np.sin(1.2 * x - 2 * np.pi * 0.4 * t)
+        elif mode == "destructive":
+            # To identiske bølger i motfase
+            y1 = np.sin(x - 2 * np.pi * 0.4 * t)
+            y2 = np.sin(x - 2 * np.pi * 0.4 * t + np.pi)
+        else:
+            raise ValueError("Mode må være 'constructive' eller 'destructive'.")
+
+        y_sum = y1 + y2
+        line1.set_data(x, y1)
+        line2.set_data(x, y2)
+        line_sum.set_data(x, y_sum)
+        return line1, line2, line_sum
+
+    ani = FuncAnimation(fig, animate, frames=frames, init_func=init, blit=True)
+    ani.save(filename, writer=PillowWriter(fps=fps))
+    plt.close(fig)
+
+# Lag GIF-ene
+constructive_path = "constructive.gif"
+destructive_path = "destructive.gif"
+
+make_interference_gif("constructive", constructive_path)
+make_interference_gif("destructive", destructive_path)
+
+# Vis i Streamlit
+st.markdown("---")
+st.subheader("📈 Konstruktiv interferens (generert)")
+st.markdown("To bølger med litt forskjellig frekvens som forsterker hverandre i perioder.")
+st.image(constructive_path, use_column_width=True)
+
+st.subheader("⚠️ Destruktiv interferens (generert)")
+st.markdown("To identiske bølger i motfase som kansellerer hverandre.")
+st.image(destructive_path, use_column_width=True)
